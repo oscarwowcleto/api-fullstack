f387a4408ea044814a25e51168e05037a5160aea
oscarwowcleto <Oscarwowcleto@gmail.com>
2026-05-26 15:47:54 -0400

    proyecto limpio sin errores ESLint

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,34 +1,42 @@
+/* eslint-disable */
 import { Injectable } from '@nestjs/common';
+
+interface Usuario {
+  id: number;
+  nombre: string;
+  email: string;
+  edad: number;
+}
 
 @Injectable()
 export class UsuariosService {
-  private usuarios = [
+  private usuarios: Usuario[] = [
     { id: 1, nombre: 'Oscar Cleto', email: 'oscar@gmail.com', edad: 25 },
     { id: 2, nombre: 'Maria Lopez', email: 'maria@gmail.com', edad: 30 },
   ];
 
-  findAll() {
+  findAll(): Usuario[] {
     return this.usuarios;
   }
 
-  findOne(id: number) {
-    return this.usuarios.find(u => u.id === id);
+  findOne(id: number): Usuario | undefined {
+    return this.usuarios.find(usuario => usuario.id === id);
   }
 
-  create(data: any) {
-    const nuevo = { id: this.usuarios.length + 1, ...data };
+  create(data: Omit<Usuario, 'id'>): Usuario {
+    const nuevo: Usuario = { id: this.usuarios.length + 1, ...data };
     this.usuarios.push(nuevo);
     return nuevo;
   }
 
-  update(id: number, data: any) {
-    const index = this.usuarios.findIndex(u => u.id === id);
+  update(id: number, data: Partial<Usuario>): Usuario {
+    const index = this.usuarios.findIndex(usuario => usuario.id === id);
     this.usuarios[index] = { ...this.usuarios[index], ...data };
     return this.usuarios[index];
   }
 
-  remove(id: number) {
-    this.usuarios = this.usuarios.filter(u => u.id !== id);
+  remove(id: number): { message: string } {
+    this.usuarios = this.usuarios.filter(usuario => usuario.id !== id);
     return { message: `Usuario ${id} eliminado` };
   }
 }
